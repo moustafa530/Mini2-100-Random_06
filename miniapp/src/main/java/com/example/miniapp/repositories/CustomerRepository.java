5f0e2688e52974d546ce3624103636ceb3d1575c
Maged Wael <icuras69@gmail.com>
2025-04-24 14:53:57 +0200

    Updated method in repo

--- miniapp/src/main/java/com/example/miniapp/repositories/CustomerRepository.java
+++ miniapp/src/main/java/com/example/miniapp/repositories/CustomerRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findCustomersByEmailDomain(String domain);
 
     // 2. Find customers by phone prefix (e.g., '+20')
-    @Query("SELECT c FROM Customer c WHERE c.phoneNumber LIKE ?1%")
-    List<Customer> findCustomersByPhonePrefix(String prefix);
+    @Query("SELECT c FROM Customer c WHERE c.phoneNumber LIKE :prefix")
+    List<Customer> findCustomersByPhonePrefix(@Param("prefix") String prefix);
 
 }
