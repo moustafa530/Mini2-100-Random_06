533f73a9b6b980a97d5ceb74aebbdd1962f8a057
Welo200 <161712606+Welo200@users.noreply.github.com>
2025-04-25 20:59:31 +0300

    captain is done

--- Mini2-100-Random_06-main/miniapp/src/main/java/com/example/miniapp/repositories/CaptainRepository.java
+++ Mini2-100-Random_06-main/miniapp/src/main/java/com/example/miniapp/repositories/CaptainRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+//done
 
 @Repository
 public interface CaptainRepository extends JpaRepository<Captain, Long> {
